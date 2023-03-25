fc000e300fd70b2f1f20f1f30c9b29118acc0208
andreivisan <andrei.m.visan@gmail.com>
2023-03-25 09:37:25 +0100

    Added design and refs to the new Note form

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -2,8 +2,8 @@ import { NoteForm } from "./NoteForm";
 
 export function NewNote() {
     return (
-        <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold">New Note</h1>
+        <div className="max-w-2xl mx-auto mt-7">
+            <h1 className="text-3xl font-bold mb-4">New Note</h1>
             <NoteForm />
         </div>
     )
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,46 +1,62 @@
+import { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import CreatableReactSelect from 'react-select/creatable';
+
 export function NoteForm() {
+    const titleRef = useRef<HTMLInputElement>(null);
+    const markdownRef = useRef<HTMLTextAreaElement>(null);
+
     return (
-        <form className="space-y-4 mt-20">
-            <div>
-                <label htmlFor="title" className="block text-sm font-medium text-gray-700">
-                    Title
-                </label>
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                />
+        <form>
+            <div className="flex flex-wrap mb-4">
+                <div className="w-full md:w-1/3 pr-4">
+                    <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+                        Title
+                    </label>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                        ref={titleRef}
+                    />
+                </div>
+
+                <div className="w-full md:w-2/3">
+                    <label htmlFor="tags" className="block text-sm font-medium text-gray-700">
+                        Tags
+                    </label>
+                    <CreatableReactSelect isMulti />
+                </div>
             </div>
 
-            <div>
-                <label htmlFor="tags" className="block text-sm font-medium text-gray-700">
-                    Tags
-                </label>
-                <input
-                    type="text"
-                    id="tags"
-                    name="tags"
-                    className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                />
-            </div>
-
-            <div>
+            <div className="mb-4">
                 <label htmlFor="content" className="block text-sm font-medium text-gray-700">
                     Content
                 </label>
                 <textarea
                     id="content"
                     name="content"
-                    rows={4}
+                    rows={15}
                     className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    ref={markdownRef}
                 ></textarea>
             </div>
 
-            <button
-                type="submit"
-                className="w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 focus:ring-offset-white"
-            >Submit</button>
+            <div className="flex justify-end">
+                <button
+                    type="submit"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+                    Save
+                </button>
+                <Link to="..">
+                    <button
+                        type="button"
+                        className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                        Cancel
+                    </button>    
+                </Link>
+        </div>
         </form>
     )
 }
